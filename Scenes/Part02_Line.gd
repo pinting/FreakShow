14e468e5e7ab4376d0e196f818c56dd71a30e748
Denes Tornyi <tornyi.denes@gmail.com>
2020-07-26 16:05:34 +0200

    Working on Part02 lines

--- Scenes/Part02_Line.gd
+++ Scenes/Part02_Line.gd
@@ -1,18 +1,25 @@
 extends "res://Objects/Selectable/Selectable.gd"
 
-export var MAX = 100
-export var STEP = 35
+# After how large value the line should disappear
+export var LIMIT = 100
 
-var count = 35
+# Size of one step
+export var STEP = 0.25
+
+# Initial value
+export var START = 0.25
+
+var current = LIMIT * START
 
 func _ready():
 	assert(STEP > 0)
 	connect("selected", self, "_on_selected")
+	material.set_shader_param("amount", current)
 
 func _on_selected():
-	count += STEP
+	current += LIMIT * STEP
 	
-	material.set_shader_param("amount", count)
+	material.set_shader_param("amount", current)
 	
-	if count >= MAX:
+	if current >= LIMIT:
 		get_parent().remove_child(self)
