3c7cb93c08b27b61b029f26c5e2a50b10bd7b213
Denes Tornyi <tornyi.denes@gmail.com>
2020-10-12 00:59:06 +0200

    Updated Buka exhibition, fixed two-player light issues

---
+++ Scenes/Part01/Part01_ExhibitionRoom.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+onready var broken_lamp_light = $LampBroken/Lamp/Light
+
+var wait = 0.0
+
+func _ready():
+	pass
+
+func _process(delta):
+	wait -= delta
+	
+	if wait <= 0.0:
+		broken_lamp_light.visible = not broken_lamp_light.visible
+		wait = Global.random_generator.randf_range(0.25, 1.0)
